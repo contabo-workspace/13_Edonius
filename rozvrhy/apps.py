ae21899b9a62ae7da2e8f82b04e18d4c43e65247
stransky <svetr6@seznam.cz>
2026-05-28 12:49:56 +0200

    basic structure

---
+++ rozvrhy/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RozvrhyConfig(AppConfig):
+    name = 'rozvrhy'
---
+++ temataky/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TematakyConfig(AppConfig):
+    name = 'temataky'
